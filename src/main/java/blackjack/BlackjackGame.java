4abdb1aee19dbc9c4ead2874bc7a9483f9f6fca4
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-03-12 11:54:02 +0900

    refactor: 불필요 BlackjackInitializer 제거

--- src/main/java/blackjack/BlackjackGame.java
+++ src/main/java/blackjack/BlackjackGame.java
@@ -1,14 +1,19 @@
 package blackjack;
 
+import blackjack.domain.Deck;
 import blackjack.domain.Players;
 import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Player;
+import blackjack.strategy.RandomShuffleStrategy;
 import blackjack.view.CardView;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class BlackjackGame {
+
+    private static final int BLACKJACK_INIT_CARD_AMOUNT = 2;
 
     private final InputView inputView;
     private final OutputView outputView;
@@ -20,13 +25,22 @@ public class BlackjackGame {
 
     public void start() {
         List<String> names = inputView.readPlayersName();
-        Dealer dealer = BlackjackInitializer.createDealer();
-        Players players = BlackjackInitializer.createPlayer(names);
+        Deck deck = new Deck(new RandomShuffleStrategy());
+        Dealer dealer = new Dealer(deck);
+        Players players = Players.of(names);
 
-        BlackjackInitializer.initializeGame(dealer, players);
+        initializeGame(dealer, players);
         printCardDistribute(dealer, players);
         requestExtraCard(dealer, players);
         outputView.printFinalResult(names, players.createResult(dealer));
+    }
+
+    public void initializeGame(final Dealer dealer, final Players players) {
+        dealer.draw(BLACKJACK_INIT_CARD_AMOUNT);
+
+        IntStream.range(0, BLACKJACK_INIT_CARD_AMOUNT)
+                .forEach(i -> players.getPlayers()
+                        .forEach(player -> player.draw(dealer.draw())));
     }
 
     private void printCardDistribute(final Dealer dealer, final Players players) {
